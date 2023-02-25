d8b7870bb0c572d42e3075fefbaf0d07bac8dc5f
eduardojigub <eduardobrunoit@gmail.com>
2023-02-25 18:57:00 -0300

    feat: create name and password states

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import Logo from '../assets/logo_primary.svg';
 import { Input } from '../components/Input';
@@ -5,6 +6,9 @@ import { Envelope, Key } from 'phosphor-react-native';
 import { Button } from '../components/Button';
 
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -18,12 +22,14 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
       <Input
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
         mb={8}
+        onChangeText={setPassword}
       />
       <Button title="Entrar" w="full" />
     </VStack>

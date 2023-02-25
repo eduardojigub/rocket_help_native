7f57eab6ed42f4e0874c1e10645135499c078013
eduardojigub <eduardobrunoit@gmail.com>
2023-02-25 18:13:30 -0300

    feat: create fontsLoaded

--- App.tsx
+++ App.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }

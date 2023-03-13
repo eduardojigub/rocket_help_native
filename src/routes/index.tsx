7573528dcdd94b712acff63b1bafba88e111fb09
eduardojigub <eduardobrunoit@gmail.com>
2023-03-13 20:22:06 -0300

    feat: update index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { SignIn } from '../screens/SignIn';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <SignIn />
+      <AppRoutes />
     </NavigationContainer>
   );
 }
